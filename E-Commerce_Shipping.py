5ce27010d98fa3f656962b67cf03264d31362c5e
moweirou <weirou.mo@ucdconnect.ie>
2021-04-24 21:42:59 +0100

    added API

--- E-Commerce_Shipping.py
+++ E-Commerce_Shipping.py
@@ -1,16 +1,3 @@
-# Real world dataset:  The aviation accident database throughout the world, from 1908-2019.
-
-# import data
-
-# Import package
-from urllib.request import urlretrieve
-
-# Assign url of file: url
-url = "https://www.kaggle.com/cgurkan/airplane-crash-data-since-1908"
-
-# Save file locally
-urlretrieve(url, 'airplane-crash-data-since-1908.csv')
-
 # Import pandas
 import pandas as pd
 
