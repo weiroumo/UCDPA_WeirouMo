a21b9ad74f08642285892f3b952213d474d48808
moweirou <weirou.mo@ucdconnect.ie>
2021-04-25 13:22:56 +0100

    added looping and iterrows to calculate gdp per capita

--- MergeDataFrames.py
+++ MergeDataFrames.py
@@ -14,3 +14,10 @@ print(pop.shape)
 gdp_pop = gdp.merge(pop, on=['Country Name', 'Country Code', 'Year'], suffixes=('_gdp', '_pop'))
 print(gdp_pop.columns)
 print(gdp_pop.shape)
+print(gdp_pop.isna().any())
+
+# Iterate over rows of gdp-pop and calculate GDP per capita
+for lab, row in gdp_pop.iterrows():
+    print(str(lab) + row['Country Name'] + str(row['Year']) + ": " + str(row['GDP']/row['Pop']))
+
+
